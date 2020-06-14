2a180a79a403e0ed3d35d6caaf245eeadec35070
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-06-14 19:43:44 +0900

    media query devaice ch

--- resources/js/tweenmax.js
+++ resources/js/tweenmax.js
@@ -31,36 +31,34 @@ TweenMax.to('.opb', 1.25,
 );
 
 // square 
-if(!navigator.userAgent.match(/(iPhone|iPad|Andoroid)/))
-{
-  var stl = new TimelineMax({ repeat: -1});
-  stl.to('.square', 8, {left: "calc(100% - 60px)"})
-  .add( 'scene1' )
-  .to( '.square', 8, {top: "calc(100% - 60px)"}, 'scene1' )
-  .add('scene2')
-  .to( '.square', 8, {left: "0%"}, 'scene2' )
-  .add('scene3')
-  .to( '.square', 8, {top: "0%"}, 'scene3' )
+var stl = new TimelineMax({ repeat: -1});
+stl.to('.square', 8, {left: "calc(100% - 60px)"})
+.add( 'scene1' )
+.to( '.square', 8, {top: "calc(100% - 60px)"}, 'scene1' )
+.add('scene2')
+.to( '.square', 8, {left: "0%"}, 'scene2' )
+.add('scene3')
+.to( '.square', 8, {top: "0%"}, 'scene3' )
 
-  var ctl = new TimelineMax({ repeat: -1,repeatDelay: 3.4});
-  ctl.to('.circle', 1.5, {height: "800px",width: "800px",opacity: 0,ease: "power4"});
+var ctl = new TimelineMax({ repeat: -1,repeatDelay: 3.4});
+ctl.to('.circle', 1.5, {height: "800px",width: "800px",opacity: 0,ease: "power4"});
 
-  $('.box').on({
-    'click': function(){
-      stl.timeScale(0.8);
-      ctl.timeScale(0.2);
-      ctl.repeatDelay(0.1);
-    }
-  });
+$('.box').on({
+  'click': function(){
+    stl.timeScale(0.8);
+    ctl.timeScale(0.2);
+    ctl.repeatDelay(0.1);
+  }
+});
 
-  $('.miniBoxes').on({
-    'click': function(){
-      stl.timeScale(1);
-      ctl.timeScale(1);
-      ctl.repeatDelay(3.4);
-    }
-  });
-}
+$('.miniBoxes').on({
+  'click': function(){
+    stl.timeScale(1);
+    ctl.timeScale(1);
+    ctl.repeatDelay(3.4);
+  }
+});
+
 // title
 $('.obj').on({
   'mouseenter': function(){
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
   <title>{{ config('app.name', 'Laravel') }}</title>
   <script src="{{ asset('js/app.js') }}" defer></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
