5f270ceb2c77b5d9d9b02abcc7daef50f3ec0c55
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-12-13 19:17:47 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,21 +5,21 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
   <title>{{ config('app.name', 'Portfolio') }}</title>
-  <script src="{{ mix('js/app.js') }}" defer></script>
+  <script src="{{ asset('js/app.js') }}" defer></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.0/gsap.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans+JP:400,700" rel="stylesheet">
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/style.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/obj.css') }}" rel="stylesheet">  
-  <link href="{{ mix('css/index.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/opening.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/header.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/about.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/skill.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/works.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/blog.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/contact.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/obj.css') }}" rel="stylesheet">  
+  <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/opening.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/about.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/skill.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/works.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/blog.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/contact.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="opb"><p>welcome</p></div>
